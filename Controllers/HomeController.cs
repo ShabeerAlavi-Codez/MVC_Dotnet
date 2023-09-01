4fd364c0ec0b48bc43446158126c3ec2e88da6af
Shabeer <shabeeralavi001@gmail.com>
2023-09-01 18:24:54 +0530

    first try of .net mvc

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Dotnet.Models;
+
+namespace MVC_Dotnet.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    // test view return
+    public IActionResult Test()
+    {
+        return View();
+    }
+    // Controllers/HomeController.cs
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
